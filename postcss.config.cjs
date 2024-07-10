c26cbb61e88b286fc8f86558082b6aceaa2a728e
ak652231 <ak65223167@gmail.com>
2024-07-10 19:59:56 +0530

    no message

--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-// postcss.config.js
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    // other plugins as needed
-  ],
-};
-
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// Example tailwind.config.js
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-  theme: {
-    extend: {}, // Extend Tailwind CSS theme here
-  },
-  plugins: [], // Additional Tailwind CSS plugins
-};
