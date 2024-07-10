ef241dd4834122162f40c9eba45fa05d7153e7cf
ak652231 <ak65223167@gmail.com>
2024-07-10 20:28:55 +0530

    no message

--- client/src/components/Why/Card.jsx
+++ client/src/components/Why/Card.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 function Card() {
   return (
-    <div className='bg-white rounded'>
-        <div className='rounded '>
-            <img src="" alt="benefits" />
+    <div class="flex justify-center items-center w-full md:w-1/2 mt-20">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg bg-dgrn body-top p-4">
+        <img class="w-full rounded" src="https://images.squarespace-cdn.com/content/v1/61042cbcc245122bc58574b7/0443839f-b9f5-4130-a6ce-79a9fcb7edbe/paw+pads.jpeg" alt="Card image cap" />
+        <div class="">
+          <div class="font-bold text-xl mb-2 top clr-mgrn">CardioqSDFGHJKLSDFCVGBHNJJSCV BNMBNM</div>
+          <p class=" text-base clr-lgrn">
+            rdfghbjnerfghhb
+          </p>
         </div>
-        <h2>Hi</h2>
-        <p>yes</p>
+      </div>
     </div>
   )
 }
